e99c493f7899de5d963fb7856c40a3ae2af46262
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-04-26 11:26:21 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+  name: 'PasqEat'
+}
+</script>
+
+<template></template>
+
+<style lang="scss">
+@use './assets/scss/style.scss';
+</style>
---
+++ src/main.js
@@ -0,0 +1,27 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+// Importiamo la libreria generica di FontAwesome
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+// Importiamo il componente di FontAwesome
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+// Importiamo le icone da utilizzare
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
+import { faCompass } from '@fortawesome/free-regular-svg-icons'
+import { faFacebook } from '@fortawesome/free-brands-svg-icons'
+
+// Icone da caricare
+library.add(faArrowLeft);
+library.add(faCompass);
+library.add(faFacebook);
+
+const app = createApp(App)
+
+// Rendo le icone disponibili globalmente
+app.component('FontAwesomeIcon', FontAwesomeIcon);
+
+app.mount('#app')
