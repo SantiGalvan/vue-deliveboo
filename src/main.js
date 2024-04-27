4c7901b44d7cfc4b695b908700c3aee960a8a2c8
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-04-27 21:06:55 +0200

    import icon

--- src/main.js
+++ src/main.js
@@ -13,7 +13,7 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 // Importiamo le icone da utilizzare
-import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
+import { faArrowLeft, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 import { faCompass } from '@fortawesome/free-regular-svg-icons'
 import { faFacebook } from '@fortawesome/free-brands-svg-icons'
 
@@ -21,7 +21,7 @@ import { faFacebook } from '@fortawesome/free-brands-svg-icons'
 import AppLoader from './components/AppLoader.vue'
 
 // Icone da caricare
-library.add(faArrowLeft);
+library.add(faArrowLeft, faMagnifyingGlass);
 library.add(faCompass);
 library.add(faFacebook);
 
