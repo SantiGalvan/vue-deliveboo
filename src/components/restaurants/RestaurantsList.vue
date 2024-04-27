eef15c302df88a9542b5a03254288898bcdc153a
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-04-27 12:48:36 +0200

    modified HomePage path into index.js

---
+++ src/components/restaurants/RestaurantsList.vue
@@ -0,0 +1,44 @@
+<script>
+export default {
+    name: 'RestaurantsList',
+    props: { restaurants: Array },
+    data: () => ({
+    }),
+    methods: {
+    },
+}
+</script>
+
+<template>
+    <div class="row mt-5 gap-3">
+        <div class="col-12" v-if="restaurants.length" v-for="restaurant in restaurants" :key="restaurant.id">
+            <!-- ProjectCard -->
+            <div class="card my-3">
+                <div class="row g-0">
+                    <div class="col">
+                        <div class="card-header d-flex justify-content-between align-items-center">
+                            <h5 class="card-title">{{ restaurant.restaurant_name }}</h5>
+                            <!-- <RouterLink v-if="!isDetail" class="btn btn-primary"
+                                :to="{ name: 'project-detail', params: { slug: project.slug } }">
+                                Vedi
+                            </RouterLink>
+                            <button v-else class="btn btn-secondary" @click="$router.back()">Torna Indietro
+                            </button> -->
+                        </div>
+                        <div class="card-body">
+                            <img v-if="restaurant.image" :src="restaurant.image" class="float-start img-fluid"
+                                :alt="restaurant.restaurant_name">
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+        <h4 v-else>Non ci sono Ristoranti</h4>
+    </div>
+
+
+</template>
+
+<style></style>
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,37 @@
+<script>
+import axios from 'axios';
+import RestaurantsList from '../components/restaurants/RestaurantsList.vue';
+const defaultEndpoint = 'http://localhost:8000/api/restaurants/';
+export default {
+    name: 'HomePage',
+    components: { RestaurantsList },
+    data: () => ({
+        restaurants: [],
+        categories: [],
+    }),
+    methods: {
+        getRestaurants() {
+            axios.get(defaultEndpoint)
+                .then(res => {
+                    const { restaurants, categories } = res.data;
+                    this.restaurants = restaurants;
+                    this.categories = categories
+                })
+                .catch(err => {
+                    console.error(err.message)
+                })
+                .then(() => { })
+        }
+    },
+    created() {
+        this.getRestaurants();
+    }
+}
+</script>
+
+<template>
+    <h1>PasqEat</h1>
+    <RestaurantsList :restaurants="restaurants" />
+</template>
+
+<style></style>
