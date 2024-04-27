6894d4a14d40d4d2be199b7304a6e890c523adcf
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-04-27 12:47:07 +0200

    Added AppLoader component

---
+++ src/components/AppLoader.vue
@@ -0,0 +1,35 @@
+<script>
+export default {
+    name: 'AppLoader',
+
+};
+</script>
+
+<template>
+    <div class="loader-overlay">
+        <div class=" spinner spinner-border text-warning" role="status">
+            <span class="visually-hidden">Loading...</span>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.loader-overlay {
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: rgba(0, 0, 0, 0.66);
+    z-index: 1;
+
+    .spinner-border {
+        width: 150px;
+        height: 150px;
+    }
+
+}
+</style>
