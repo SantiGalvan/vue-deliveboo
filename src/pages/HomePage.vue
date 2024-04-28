f2ab617b59b7bed77b9014d6e6fae40e0d50780a
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-04-29 00:08:01 +0200

    added element into home hero

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,126 +1,100 @@
 <script>
 export default {
-    name: 'HomePage'
+    name: 'HomeHero'
 }
 </script>
 
 <template>
-    <div class="container-md pb-4">
-        <div class="row">
-            <div class="col-12 col-md-6 col-xl-4 mt-4">
-                <div class="home-card home-categories">
-                    <h3>Cerca i ristoranti per categoria</h3>
-                    <!-- Da cambiare in RouterLink -->
-                    <a href="">Categorie</a>
+    <div class="hero">
+        <div class="container">
+            <div class="row align-items-center">
+
+                <!-- Immagine -->
+                <div class="col-12 col-lg-6 text-center">
+                    <img src="../assets/img/8219559_delivery_express_vespa_scooter_deliver_icon_1.png"
+                        alt="Immagine Hero" class="hero-img">
                 </div>
-            </div>
-            <div class="col-12 col-md-6 col-xl-4 mt-4 align-self-xl-end">
-                <div class="home-card home-search">
-                    <h3>Cerca i ristoranti per nome</h3>
-                    <!-- Da cambiare in RouterLink -->
-                    <a href="">
-                        <FontAwesomeIcon icon="fa-solid fa-magnifying-glass" /> Cerca
-                    </a>
+
+                <!-- Testo -->
+                <div class="hero-col col-12 col-lg-6 text-center p-3">
+                    <h1>Consegna di cibo a domicilio dai tuoi ristoranti preferiti</h1>
+                    <p>I migliori ristoranti li trovi solo su PasqEat</p>
+
+                    <!-- Cambiare il buttone -->
+                    <button class="btn btn-success">Ristoranti
+                        <FontAwesomeIcon icon="fa-solid fa-arrow-down" class="fa-bounce" />
+                    </button>
                 </div>
-            </div>
-            <div class="offset-md-3 offset-xl-0 col-md-6 col-xl-4 d-none d-md-flex  mt-4">
-                <div class="home-card home-restaurants">
-                    <h3>Visualizza tutti i ristoranti</h3>
-                    <!-- Da cambiare in RouterLink -->
-                    <a href="">
-                        Ristoranti
-                    </a>
-                </div>
+
             </div>
         </div>
     </div>
-    <div class="slider"></div>
 </template>
 
 <style lang="scss">
-.row {
-    height: 400px;
-}
+.hero {
+    // Background da cambiare
+    background-color: gray;
 
-.home-card {
-    height: 150px;
-    padding: 10px;
-    background-color: firebrick;
-    border-radius: 20px;
-
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    gap: 20px;
-
-    a {
-        text-decoration: none;
-        color: black;
-
-        padding: 10px 15px;
-        background-color: white;
-        border-radius: 10px;
-    }
-
-    h3 {
-        margin-bottom: 0;
-        text-transform: uppercase;
-        color: #fff;
-        text-shadow: 3px 0px 7px rgba(46, 38, 12, 0.8), -3px 0px 7px rgba(42, 35, 11, 0.8), 0px 4px 7px rgba(45, 37, 11, 0.8);
-
-    }
-}
-
-.home-categories {
-    background: url(https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1);
-    background-size: cover;
-}
-
-.home-search {
-    background: url(https://images.pexels.com/photos/769289/pexels-photo-769289.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1);
-    background-size: cover;
-}
-
-.home-restaurants {
-    background: url(https://images.pexels.com/photos/239581/pexels-photo-239581.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1);
-    background-size: cover;
-}
-
-.slider {
-    height: 250px;
-    background-color: goldenrod;
-}
-
-@media screen and (min-width: 768px) {
-    .row {
-        height: 450px;
-    }
-
-    .home-card {
+    .col-12 {
         height: 200px;
-        flex-direction: column;
+        margin-top: 10px;
+        margin-bottom: 10px;
+        overflow: hidden;
         text-align: center;
+        color: rgb(233, 229, 229);
+
+        .hero-img {
+            height: 100%;
+            width: auto;
+        }
+    }
+
+    .hero-col {
+        h1 {
+            font-size: 2rem;
+        }
+
+        p {
+            font-size: 1.1rem;
+        }
+    }
+}
+
+@media screen and (min-width: 992px) {
+
+    .hero {
+        .col-lg-6 {
+            height: 300px;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+        }
     }
 }
 
 @media screen and (min-width: 1200px) {
-    .row {
-        height: 500px;
-    }
 
-    .home-card {
-        height: 300px;
-        flex-direction: column;
-        text-align: center;
-    }
+    .hero {
+        .col-lg-6 {
+            height: 600px;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+        }
 
-    .col-xl-4 {
-        padding-top: 30px;
-        padding-bottom: 30px;
-    }
+        .hero-col {
+            h1 {
+                font-size: 3.5rem;
+            }
 
-    .slider {
-        height: 300px;
+            p {
+                font-size: 1.4rem;
+                margin-bottom: 20px;
+            }
+        }
     }
 }
 </style>
