0fe20a641b20dca20fe2eefa6254d85c9d24d04d
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-04-28 17:12:35 +0200

    added component CategoriesList

---
+++ src/components/categories/CategoriesList.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+    name: 'CategoriesList',
+    props: { categories: Array },
+    data: () => ({
+    }),
+    methods: {
+    },
+}
+</script>
+
+<template>
+    <!--Lista Categorie-->
+    <ul class="d-flex list-unstyled gap-3 justify-content-center flex-wrap py-5">
+        <!--Categoria-->
+        <RouterLink :to="{ name: 'category-restaurants', params: { id: category.id } }" v-for="category in categories"
+            :key="category.id" @click="$emit('get-restaurants', category.id)" class="text-decoration-none text-dark">
+            <li>
+                {{ category.label }}
+            </li>
+        </RouterLink>
+    </ul>
+</template>
+
+<style></style>
