6269eadb8aec8b945f37a4b852b22afeead89a6c
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-05-01 14:55:20 +0200

    added method closeBadge

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -12,12 +12,11 @@ export default {
     data: () => ({
         restaurants: [],
         categories: [],
-        selectedCategories: [],
+        selectedCategoriesLabel: [],
     }),
     methods: {
         //Chiamata axios per ricevere i ristoranti e le categorie
         getRestaurants(slug) {
-
             if (slug) {
                 endpoint = `http://localhost:8000/api/categories/${slug}/restaurants`;
             } else {
@@ -35,37 +34,35 @@ export default {
                 })
                 .then(() => { })
         },
-
-        //Funzione per rendere la prima lettera maiuscola
-        capitalize(string) {
-            if (!string) {
-                return '';
-            }
-
-            return string.charAt(0).toUpperCase() + string.slice(1);
+        //Funzione per rimuovere una categoria da quelle selezionate
+        closeBadge(label) {
+            this.selectedCategoriesLabel = this.selectedCategoriesLabel.filter(category => {
+                console.log(label, category)
+                return category != label
+            })
         }
     },
     computed: {
         //Computed per filtrare i ristoranti in base una o più categorie
         filteredRestaurants() {
             //Controllo se non ci sono categorie selezionate
-            if (this.selectedCategories.length === 0) {
+            if (this.selectedCategoriesLabel.length === 0) {
                 //Restituisco tutti i Ristoranti
                 return this.restaurants;
             } else {
                 //Faccio un filtro su restaurants
                 return this.restaurants.filter(restaurant =>
                     //Controllo se possiede tutte le categorie selezionate
-                    this.selectedCategories.every(selectedCategoryId =>
+                    this.selectedCategoriesLabel.every(selectedCategoryLabel =>
                         /*Controllo se le categorie del ristorante 
                         corrispondono all'id di una o più categoria selezionata*/
                         restaurant.categories.some(category =>
-                            category.id === selectedCategoryId
+                            category.label === selectedCategoryLabel
                         )
                     )
                 );
             }
-        }
+        },
     },
     created() {
         this.getRestaurants();
@@ -80,10 +77,10 @@ export default {
     <section class="container-fluid container-lg mb-5" id="home-page">
         <div class="d-lg-none upper-categories-filter">
             <!--Lista Categorie-->
-            <div>
+            <div class="d-flex flex-wrap gap-3 mt-4 justify-content-center">
                 <div v-for="category in categories" :key="category.id">
-                    <input type="checkbox" :value="category.id" v-model="selectedCategories">
-                    <label>{{ category.label }}</label>
+                    <input type="checkbox" :value="category.label" v-model="selectedCategoriesLabel">
+                    <label class="ms-2">{{ category.label }}</label>
                 </div>
             </div>
         </div>
@@ -96,15 +93,24 @@ export default {
                     <!--Lista Categorie-->
                     <div class="mt-4">
                         <div v-for="category in categories" :key="category.id">
-                            <input type="checkbox" :value="category.id" v-model="selectedCategories">
+                            <input type="checkbox" :value="category.label" v-model="selectedCategoriesLabel">
                             <label class="ms-2">{{ category.label }}</label>
                         </div>
                     </div>
                 </div>
             </nav>
-            <div class="w-100">
+            <div class="w-100 text-center text-lg-start">
                 <h1 class="d-inline">Ristoranti</h1>
-                <h1 class="d-inline" v-if="category">: {{ capitalize(category) }}</h1>
+                <div class="d-flex gap-2 button-dismissable justify-content-center justify-content-lg-start"
+                    v-if="selectedCategoriesLabel.length">
+                    <!--Badge-->
+                    <div class="btn btn-primary default-cursor mt-2 d-flex align-items-center"
+                        v-for="category in selectedCategoriesLabel" :key="category">
+                        {{ category }}
+                        <button type="button" class="btn btn-sm btn-close" data-bs-dismiss="button" aria-label="Close"
+                            @click="closeBadge(category)"></button>
+                    </div>
+                </div>
                 <!--Lista dei Ristoranti-->
                 <RestaurantsList :filteredRestaurants="filteredRestaurants" />
             </div>
@@ -116,6 +122,10 @@ export default {
 .main-content {
     display: flex;
     gap: 20px;
+}
+
+.default-cursor {
+    cursor: default;
 }
 
 .side-bar {
