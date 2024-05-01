8da34f241eac4723a5e90ba3c95d3d853adbdc30
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-05-01 19:43:54 +0200

    added route restaurant-show

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@
 import { createRouter, createWebHistory } from 'vue-router';
 //Importo le Pagine
 import HomePage from '../pages/HomePage.vue';
+import RestaurantShow from '../pages/RestaurantShow.vue'
+// import RestaurantsList from '../components/restaurants/RestaurantsList.vue';
 
 //Definisco le rotte
 const router = createRouter({
@@ -12,7 +14,10 @@ const router = createRouter({
     routes: [
         { path: '/', name: 'home', component: HomePage },
         { path: '/restaurants', name: 'restaurants', component: HomePage },
-        { path: '/categories/:slug/restaurants', name: 'category-restaurants', component: HomePage },
+        {
+            path: '/restaurants/:slug', name: 'restaurant-show', component: RestaurantShow
+        },
+        // { path: '/categories/:slug/restaurants', name: 'category-restaurants', component: HomePage },
     ]
 });
 export { router }
