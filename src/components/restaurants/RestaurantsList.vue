82c11f1211775b8b2886f58a7b0f330b0b4d004b
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-05-01 19:44:39 +0200

    added RouterLink

--- src/components/restaurants/RestaurantsList.vue
+++ src/components/restaurants/RestaurantsList.vue
@@ -1,4 +1,6 @@
 <script>
+import { RouterLink } from 'vue-router';
+
 export default {
     name: 'RestaurantsList',
     props: { filteredRestaurants: Array },
@@ -13,20 +15,22 @@ export default {
     <div class="row mt-3">
         <div class="col-12 col-md-6 col-lg-4" v-if="filteredRestaurants?.length"
             v-for="restaurant in filteredRestaurants" :key="restaurant.id">
-            <!-- ProjectCard -->
-            <div class="card my-3">
-                <div class="row">
-                    <div class="col-12">
-                        <div class="card-header d-flex justify-content-between align-items-center">
-                            <h5 class="card-title">{{ restaurant.restaurant_name }}</h5>
-                        </div>
-                        <div class="card-body">
-                            <img v-if="restaurant.image" :src="`http://127.0.0.1:8000/storage/${restaurant.image}`"
-                                class="float-start img-fluid" :alt="restaurant.restaurant_name">
+            <!-- RestaurantctCard -->
+            <RouterLink :to="{ name: 'restaurant-show', params: { slug: restaurant.slug } }">
+                <div class="card my-3">
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="card-header d-flex justify-content-between align-items-center">
+                                <h5 class="card-title">{{ restaurant.restaurant_name }}</h5>
+                            </div>
+                            <div class="card-body">
+                                <img v-if="restaurant.image" :src="`http://127.0.0.1:8000/storage/${restaurant.image}`"
+                                    class="float-start img-fluid" :alt="restaurant.restaurant_name">
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            </RouterLink>
         </div>
         <h4 v-else>Non ci sono Ristoranti</h4>
     </div>
