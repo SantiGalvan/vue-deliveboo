34c761fd737431b2b003b85f2a4f6d87838699b5
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-05-02 10:43:17 +0200

    added container

--- src/pages/RestaurantShow.vue
+++ src/pages/RestaurantShow.vue
@@ -35,35 +35,36 @@ export default {
 </script>
 
 <template>
-    <h1>Ciao</h1>
-    <!--Informazioni del Ristorante-->
-    <ul>
-        <li>{{ restaurant.restaurant_name }}</li>
-        <li>{{ restaurant.city }}</li>
-        <li>{{ restaurant.address }}</li>
-        <li>{{ restaurant.cap }}</li>
-        <li>{{ restaurant.phone }}</li>
-        <li>{{ restaurant.image }}</li>
-    </ul>
+    <section class="container-fluid container-lg mb-5" id="restaurant-detail">
+        <!--Informazioni del Ristorante-->
+        <ul>
+            <li>{{ restaurant.restaurant_name }}</li>
+            <li>{{ restaurant.city }}</li>
+            <li>{{ restaurant.address }}</li>
+            <li>{{ restaurant.cap }}</li>
+            <li>{{ restaurant.phone }}</li>
+            <li>{{ restaurant.image }}</li>
+        </ul>
 
-    <div class="row mt-3">
-        <div class="col-12 col-md-6 col-lg-4" v-for="dish in dishes" :key="dish.id">
-            <!-- Dish Card --> <!--Questo sarà un componente Card-->
-            <div class="card my-3">
-                <div class="row">
-                    <div class="col-12">
-                        <div class="card-header d-flex justify-content-between align-items-center">
-                            <h5 class="card-title">{{ dish.name }}</h5>
-                        </div>
-                        <div class="card-body">
-                            <img v-if="dish.image" :src="`http://127.0.0.1:8000/storage/${dish.image}`"
-                                class="float-start img-fluid" :alt="dish.name">
+        <div class="row mt-3">
+            <div class="col-12 col-md-6 col-lg-4" v-for="dish in dishes" :key="dish.id">
+                <!-- Dish Card --> <!--Questo sarà un componente Card-->
+                <div class="card my-3">
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="card-header d-flex justify-content-between align-items-center">
+                                <h5 class="card-title">{{ dish.name }}</h5>
+                            </div>
+                            <div class="card-body">
+                                <img v-if="dish.image" :src="`http://127.0.0.1:8000/storage/${dish.image}`"
+                                    class="float-start img-fluid" :alt="dish.name">
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
+    </section>
 </template>
 
 <style></style>
