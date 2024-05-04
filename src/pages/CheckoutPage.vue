7a596a3124ae8415fa83277512a4f7f26ad3429b
Your Name <you@example.com>
2024-05-04 16:37:08 +0200

    created store

---
+++ src/pages/CheckoutPage.vue
@@ -0,0 +1,25 @@
+<script>
+import { store } from '../store';
+
+//import CartCanvas from '../components/CartCanvas.vue';
+
+export default {
+    name: 'CheckoutPage',
+    components: {},
+    data: () => ({
+        cartDishes : store.cartDishes,
+    }),
+
+}
+</script>
+
+<template>
+    <h1>Checkout</h1>
+
+   
+
+</template>
+
+<style lang="scss" scoped>
+
+</style>
