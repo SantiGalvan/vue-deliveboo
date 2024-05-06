bb39742fcec1c176dcf285744434b5a67c7f2e50
GioacchinoArgo <gioacchinogabri.argo@libero.it>
2024-05-06 23:14:29 +0200

    added restaurant label to restaurant card

--- src/components/restaurants/RestaurantsList.vue
+++ src/components/restaurants/RestaurantsList.vue
@@ -3,11 +3,7 @@ import { RouterLink } from 'vue-router';
 
 export default {
     name: 'RestaurantsList',
-    props: { restaurants: Array },
-    data: () => ({
-    }),
-    methods: {
-    },
+    props: { restaurants: Array }
 }
 </script>
 
@@ -15,16 +11,24 @@ export default {
     <div class="row mt-3">
         <div class="col-12 col-sm-6 col-md-6 col-lg-4 my-3" v-if="restaurants?.length" v-for="restaurant in restaurants"
             :key="restaurant.id">
-
-            <!-- RestaurantctCard -->
+            <!-- RestaurantctCard --> <!--Questo sarà un componente Card-->
             <RouterLink class="text-decoration-none"
                 :to="{ name: 'restaurant-show', params: { slug: restaurant.slug } }">
-                <div class="card rounded-4">
+                <div class="card rounded-3">
                     <img v-if="restaurant.image" :src="restaurant.image" class="img-fluid"
                         :alt="restaurant.restaurant_name">
                 </div>
-                <h5 class="mt-3 ms-2 text-black">{{ restaurant.restaurant_name }}</h5>
             </RouterLink>
+            <div class="d-flex align-items-center justify-content-between">
+                <h5 class="mt-3 ms-2 text-black">{{ restaurant.restaurant_name }}</h5>
+                <ul class="d-flex gap-3 align-items-center mb-0 list-filter list-unstyled">
+                    <li v-for="category in restaurant.categories">
+                        <div class="catogry-label">
+                            <img :src="category.img" :alt="category.label" class="category-image-sm">
+                        </div>
+                    </li>
+                </ul>
+            </div>
         </div>
         <h4 v-else>Non ci sono Ristoranti</h4>
     </div>
