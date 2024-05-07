cb36361cccbc5f5a0c2f2ba034ba5475ebcdeb5c
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-05-07 23:39:58 +0200

    modified PaymentComponent

--- src/components/cart/PaymentComponent.vue
+++ src/components/cart/PaymentComponent.vue
@@ -12,11 +12,7 @@ export default {
 </script>
 
 <template>
-    <form id="payment-form" action="http://127.0.0.1:8000/api/orders/make/payment" method="post">
-        <div id="dropin-container"></div>
-        <input type="submit" />
-        <input type="hidden" id="nonce" name="payment_method_nonce" />
-    </form>
+
 
 </template>
 
