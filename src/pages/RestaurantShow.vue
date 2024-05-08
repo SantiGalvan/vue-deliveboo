4c953fa7cb5b8c969dde3a47de327a773e07730d
GioacchinoArgo <gioacchinogabri.argo@libero.it>
2024-05-08 14:04:15 +0200

    fixed checkout page buttons

--- src/pages/RestaurantShow.vue
+++ src/pages/RestaurantShow.vue
@@ -311,6 +311,7 @@ export default {
     color: #fff;
     font-weight: 600;
     text-shadow: 0px 1px 3px rgba(0, 0, 0, 0.7);
+    border: none;
 
     background-color: #ffb10a;
     box-shadow: rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px;
